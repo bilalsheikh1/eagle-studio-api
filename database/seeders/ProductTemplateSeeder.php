cbb589926df7bb3c6263a5ab320260ba1b85a33c
bilalsheikh1 <bilalsheikh923@gmail.com>
2021-12-02 23:25:06 +0500

    products resource route move under middlewear

---
+++ database/seeders/ProductTemplateSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ProductTemplate;
+use Illuminate\Database\Seeder;
+
+class ProductTemplateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ProductTemplate::query()->updateOrInsert(['name' => 'Game', 'urn' => 'game-template'],
+            ['name' => 'Game', 'urn' => 'game-template']);
+        ProductTemplate::query()->updateOrInsert(['name' => 'App', 'urn' => 'app-template'],
+            ['name' => 'App', 'urn' => 'app-template']);
+    }
+}
