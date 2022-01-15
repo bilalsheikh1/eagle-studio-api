c4bba28e024ffdfd5728fa7ad740086f4b1c3d51
bilalsheikh1 <bilalsheikh923@gmail.com>
2022-01-16 01:07:24 +0500

    add product category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
+        $this->call(ProductCategorySeeder::class);
         $this->call(ProductTemplateSeeder::class);
         $this->call(SystemSettingSeeder::class);
     }
